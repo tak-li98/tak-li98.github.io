fd1f9e06e259839ae5be569fbf1b879ad251e111
Tak Li <takwing.li98@gmail.com>
2021-08-30 18:47:16 +0100

    Just Skill page left to do

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,5 +1,6 @@
 function OpenSocietyInfo() {
-    window.alert("button works");
+    var card = document.getElementById("contactcard");
+    card.style.opacity = 1;
 }
 function DisplayDetails() {
     var img_id = document.getElementById('masterimage');
